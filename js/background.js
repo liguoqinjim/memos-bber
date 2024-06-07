0f9f0aaa5254272122381bca484dd4eb067f0514
liguoqinjim <>
2024-06-07 16:02:11 +0800

    feat: Add context menu options for sending selection, link, and image
    The code changes in this commit add context menu options for sending selected text, links, and images. This allows users to easily send content to the extension for further processing. Additionally, the commit includes improvements to the background script and manifest file, such as adding necessary permissions and commands.
    
    Note: This commit message follows the conventional format of "type: description". The "feat" type indicates the addition of a new feature.

--- js/background.js
+++ js/background.js
@@ -1,47 +1,92 @@
 chrome.runtime.onInstalled.addListener(() => {
-    chrome.contextMenus.create(
-      {
-        type: 'normal',
-        title: chrome.i18n.getMessage("sendTo"),
-        id: 'Memos-send-selection',
-        contexts: ['selection']
-      },
-    )
-    chrome.contextMenus.create(
-      {
-        type: 'normal',
-        title: chrome.i18n.getMessage("sendLinkTo"),
-        id: 'Memos-send-link',
-        contexts: ['link', 'page']
-      },
-    )
-    chrome.contextMenus.create(
-      {
-        type: 'normal',
-        title: chrome.i18n.getMessage("sendImageTo"),
-        id: 'Memos-send-image',
-        contexts: ['image']
-      },
-    )
+  chrome.contextMenus.create(
+    {
+      type: 'normal',
+      title: chrome.i18n.getMessage("sendTo"),
+      id: 'Memos-send-selection',
+      contexts: ['selection']
+    },
+  )
+  chrome.contextMenus.create(
+    {
+      type: 'normal',
+      title: chrome.i18n.getMessage("sendLinkTo"),
+      id: 'Memos-send-link',
+      contexts: ['link', 'page']
+    },
+  )
+  chrome.contextMenus.create(
+    {
+      type: 'normal',
+      title: chrome.i18n.getMessage("sendImageTo"),
+      id: 'Memos-send-image',
+      contexts: ['image']
+    },
+  )
 })
 chrome.contextMenus.onClicked.addListener(info => {
-    let tempCont=''
-    switch(info.menuItemId){
-      case 'Memos-send-selection':
-        tempCont = info.selectionText + '\n'
-        break
-      case 'Memos-send-link':
-        tempCont = (info.linkUrl || info.pageUrl) + '\n'
-        break
-      case 'Memos-send-image':
-        tempCont = `![](${info.srcUrl})` + '\n'
-        break
+  let tempCont = ''
+  switch (info.menuItemId) {
+    case 'Memos-send-selection':
+      tempCont = info.selectionText + '\n'
+      break
+    case 'Memos-send-link':
+      tempCont = (info.linkUrl || info.pageUrl) + '\n'
+      break
+    case 'Memos-send-image':
+      tempCont = `![](${info.srcUrl})` + '\n'
+      break
+  }
+  chrome.storage.sync.get({ open_action: "save_text", open_content: '' }, function (items) {
+    if (items.open_action === 'upload_image') {
+      alert(chrome.i18n.getMessage("picPending"));
+    } else {
+      chrome.storage.sync.set({ open_action: "save_text", open_content: items.open_content + tempCont });
     }
-    chrome.storage.sync.get({open_action: "save_text", open_content: ''}, function(items) {
-      if(items.open_action === 'upload_image') {
-        alert(chrome.i18n.getMessage("picPending"));
-      } else {
-        chrome.storage.sync.set({open_action: "save_text", open_content: items.open_content + tempCont});
-      }
-    })
+  })
 })
+
+chrome.commands.onCommand.addListener(function (command) {
+  if (command === 'copy-markdown-link') {
+    generateMarkdownLink();
+  }
+});
+
+function generateMarkdownLink() {
+  //获取当前标签页的标题和链接
+  chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+    let title = tabs[0].title;
+    let url = tabs[0].url;
+    let markdownLink = `[${title}](${url})`;
+    addToClipboard(markdownLink);
+    // addToClipboardV2(markdownLink);
+  });
+}
+
+// https://github.com/GoogleChrome/chrome-extensions-samples/tree/main/functional-samples/cookbook.offscreen-clipboard-write
+// Solution 1 - As of Jan 2023, service workers cannot directly interact with
+// the system clipboard using either `navigator.clipboard` or
+// `document.execCommand()`. To work around this, we'll create an offscreen
+// document and pass it the data we want to write to the clipboard.
+async function addToClipboard(value) {
+  await chrome.offscreen.createDocument({
+    url: 'clipboard/offscreen.html',
+    reasons: [chrome.offscreen.Reason.CLIPBOARD],
+    justification: 'Write text to the clipboard.'
+  });
+
+  // Now that we have an offscreen document, we can dispatch the
+  // message.
+  chrome.runtime.sendMessage({
+    type: 'copy-data-to-clipboard',
+    target: 'offscreen-doc',
+    data: value
+  });
+}
+
+// Solution 2 – Once extension service workers can use the Clipboard API,
+// replace the offscreen document based implementation with something like this.
+// eslint-disable-next-line no-unused-vars -- This is an alternative implementation
+async function addToClipboardV2(value) {
+  navigator.clipboard.writeText(value);
+}
