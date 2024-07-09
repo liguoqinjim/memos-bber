65dbd84d25bea3e7bf7246bb985ac4cc71586742
liguoqinjim <liguoqinjim23@gmail.com>
2024-07-09 13:51:05 +0800

    feat($): 1.prop,2.clipboard

--- js/background.js
+++ js/background.js
@@ -45,3 +45,48 @@ chrome.contextMenus.onClicked.addListener(info => {
       }
     })
 })
+
+chrome.commands.onCommand.addListener(function (command) {
+  if (command === 'copy-markdown-link') {
+    generateMarkdownLink();
+  }
+});
+
+function generateMarkdownLink() {
+  //获取当前标签页的标题和链接
+  chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+    let title = tabs[0].title;
+    let url = tabs[0].url;
+    let markdownLink = `[${title}](${url})`;
+    addToClipboard(markdownLink);
+    // addToClipboardV2(markdownLink);
+  });
+}
+
+// https://github.com/GoogleChrome/chrome-extensions-samples/tree/main/functional-samples/cookbook.offscreen-clipboard-write
+// Solution 1 - As of Jan 2023, service workers cannot directly interact with
+// the system clipboard using either `navigator.clipboard` or
+// `document.execCommand()`. To work around this, we'll create an offscreen
+// document and pass it the data we want to write to the clipboard.
+async function addToClipboard(value) {
+  await chrome.offscreen.createDocument({
+    url: 'clipboard/offscreen.html',
+    reasons: [chrome.offscreen.Reason.CLIPBOARD],
+    justification: 'Write text to the clipboard.'
+  });
+
+  // Now that we have an offscreen document, we can dispatch the
+  // message.
+  chrome.runtime.sendMessage({
+    type: 'copy-data-to-clipboard',
+    target: 'offscreen-doc',
+    data: value
+  });
+}
+
+// Solution 2 – Once extension service workers can use the Clipboard API,
+// replace the offscreen document based implementation with something like this.
+// eslint-disable-next-line no-unused-vars -- This is an alternative implementation
+async function addToClipboardV2(value) {
+  navigator.clipboard.writeText(value);
+}
---
+++ js/popup.js
@@ -0,0 +1,30 @@
+function toBottom() {
+    var tc = document.getElementById("content");
+    tc.focus();
+
+    tc.scrollTop = tc.scrollHeight;
+}
+
+function popupAuto() {
+    //清空输入框
+    $("textarea[name=text]").val('')
+
+    //获取tab页内容
+    chrome.tabs.query({ active: true, currentWindow: true }, ([tab]) => {
+        var linkHtml = " [" + tab.title + "](" + tab.url + ") "
+        if (tab.url) {
+            add(linkHtml);
+
+            //移动光标到最后
+            toBottom();
+        } else {
+            $.message({
+                message: chrome.i18n.getMessage("getTabFailed")
+            })
+        }
+    })
+}
+
+window.onload = function () {
+    popupAuto();
+};
